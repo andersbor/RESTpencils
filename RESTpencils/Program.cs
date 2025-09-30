23161e4fa75d0cdd1330e7054d4340cf9f4e4fd1
andersbor <anbo@zealand.dk>
2025-09-30 15:35:34 +0200

    CORS added to program.cs

--- RESTpencils/Program.cs
+++ RESTpencils/Program.cs
@@ -9,6 +9,26 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<PencilsRepository>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("allowAnythingFromZealand",
+                builder =>
+                    builder.WithOrigins("http://zealand.dk")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+    options.AddPolicy("allowGetPut",
+                    builder =>
+                        builder.AllowAnyOrigin()
+                        .WithMethods("GET", "PUT")
+                        .AllowAnyHeader());
+    options.AddPolicy("allowAnything", // similar to * in Azure
+        builder =>
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +40,7 @@ var app = builder.Build();
 }
 
 app.UseAuthorization();
+app.UseCors("allowGetPut");
 
 app.MapControllers();
 
